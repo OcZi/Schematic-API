ca42830f3f4e035bc1aa694a1e51e102295d4a90
salva <atlas200x@gmail.com>
2022-07-29 00:18:15 -0500

    refactor(ChildTagUtil): change runtime exceptions for IOExceptions

--- src/main/java/me/oczi/schematic/utils/ChildTagUtil.java
+++ src/main/java/me/oczi/schematic/utils/ChildTagUtil.java
@@ -2,17 +2,20 @@ package me.oczi.schematic.utils;
 
 import org.jnbt.Tag;
 
+import java.io.IOException;
 import java.util.Map;
 
 public interface ChildTagUtil {
 
-    static <T extends Tag> T getChildTag(Map<String, Tag> items, String key, Class<T> expected) {
+    static <T extends Tag> T getChildTag(Map<String, Tag> items,
+                                         String key,
+                                         Class<T> expected) throws IOException {
         if (!items.containsKey(key)) {
-            throw new IllegalArgumentException("Schematic file need the \"" + key + "\" tag");
+            throw new IOException("Schematic file need the \"" + key + "\" tag");
         }
         Tag tag = items.get(key);
         if (!expected.isInstance(tag)) {
-            throw new IllegalArgumentException(key + " tag is not of tag type " + expected.getName());
+            throw new IOException(key + " tag is not of tag type " + expected.getName());
         }
         return expected.cast(tag);
     }
